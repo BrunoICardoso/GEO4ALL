889cd305ca97f64b75a482ccfcea82c856a4dc33
BrunoICardoso <bruno.inacio88@gmail.com>
2019-07-07 16:05:57 -0300

    Melhorias

--- RN/ETLDataPoints.py
+++ RN/ETLDataPoints.py
@@ -1,94 +1,190 @@
 import codecs
 from decimal import *
 
-# Essa Função Guarda toda a regra de extração dos arquivos data_points_20180101
+
+# Essa Classes Guarda toda a regra de extração dos arquivos "data_points_"
 # Conforme o algoritmo tem a função de guardar os valores como objeto como Latitude,Longitude,Distance,Bearing
 # Tambem se percebe que faltava valores como por exemplo Latidade onde o algorismo faz dele um valor NULO
 # Solução para problema dos valores restantes é quando um valor voltasse a repetir é nesse caso se salvaria aquele valor.
-def ExtracDataPoint(file) -> list:
-    DataCamp = {'Latitude': None, 'Longitude': None, 'Distance': None, 'Bearing': None}
-    Campadd = {'Latitude': False, 'Longitude': False, 'Distance': False, 'Bearing': False}
-    ListSave = []
 
-    with codecs.open(file, "r", "utf-8") as readFileDataPoints:
+class DataPoint:
 
-        ListDataPoints = [line.replace("\n", "").replace("Bearing", "\nBearing") for line in readFileDataPoints]
+    def __init__(self, latitude: Decimal = None, longitude: Decimal = None, distance: str = None, bearing: str = None):
+        self._latitude = latitude
+        self._longitude = longitude
+        self._distance = distance
+        self._bearing = bearing
 
-        for line in ListDataPoints:
-            linha = line.split("\n")
+    @property
+    def Latitude(self):
+        return self._latitude
 
-            for valor in linha:
-                lista = valor.split(":")
+    @Latitude.setter
+    def Latitude(self, latitude):
+        self._latitude = latitude
 
-                if lista[0] == "Latitude":
+    @property
+    def Longitude(self):
+        return self._longitude
 
-                    if not Campadd['Latitude']:
-                        DataCamp['Latitude'] = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
-                        Campadd['Latitude'] = True
+    @Longitude.setter
+    def Longitude(self, longitude):
+        self._longitude = longitude
 
-                    elif DataCamp['Latitude'] != None:
-                        ListSave.append(DataCamp)
-                        Latitude = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
-                        DataCamp = {'Latitude': Latitude, 'Longitude': None, 'Distance': None, 'Bearing': None}
-                        Campadd = {'Latitude': True, 'Longitude': False, 'Distance': False, 'Bearing': False}
+    @property
+    def Distance(self):
+        return self._distance
 
-                    continue
+    @Distance.setter
+    def Distance(self, distance):
+        self._distance = distance
 
-                elif not Campadd['Latitude']:
-                    Campadd['Latitude'] = True
+    @property
+    def Bearing(self):
+        return self._bearing
 
-                if lista[0] == "Longitude":
-                    if not Campadd['Longitude'] or Campadd['Longitude'] == None:
-                        DataCamp['Longitude'] = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
-                        Campadd['Longitude'] = True
+    @Bearing.setter
+    def Bearing(self, bearing):
+        self._bearing = bearing
 
-                    elif DataCamp['Longitude'] != None:
-                        ListSave.append(DataCamp)
-                        Longitude = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
-                        DataCamp = {'Latitude': None, 'Longitude': Longitude, 'Distance': None, 'Bearing': None}
-                        Campadd = {'Latitude': False, 'Longitude': True, 'Distance': False, 'Bearing': False}
 
-                    continue
+class ValidateDataPoint:
 
-                elif not Campadd['Longitude']:
-                    Campadd['Longitude'] = True
+    def __init__(self, latitude: bool = False, longitude: bool = False, distance: bool = False, bearing: bool = False):
+        self._latitude = latitude
+        self._longitude = longitude
+        self._distance = distance
+        self._bearing = bearing
 
-                if lista[0] == "Distance":
+    @property
+    def Latitude(self):
+        return self._latitude
 
-                    if not Campadd['Distance']:
-                        DataCamp['Distance'] = lista[1].replace("  ", "").lstrip()
-                        Campadd['Distance'] = True
+    @Latitude.setter
+    def Latitude(self, latitude):
+        self._latitude = latitude
 
-                    elif DataCamp['Distance'] != None:
-                        ListSave.append(DataCamp)
-                        Distance = lista[1].replace("  ", "").lstrip()
-                        DataCamp = {'Latitude': None, 'Longitude': None, 'Distance': Distance, 'Bearing': None}
-                        Campadd = {'Latitude': False, 'Longitude': False, 'Distance': True, 'Bearing': False}
+    @property
+    def Longitude(self):
+        return self._longitude
 
-                    continue
+    @Longitude.setter
+    def Longitude(self, longitude):
+        self._longitude = longitude
 
-                elif not Campadd['Distance']:
-                    Campadd['Distance'] = True
+    @property
+    def Distance(self):
+        return self._distance
 
-                if lista[0] == "Bearing":
-                    if not Campadd['Bearing'] or Campadd['Bearing'] == None:
-                        DataCamp['Bearing'] = lista[1].replace("  ", "").lstrip()
-                        Campadd['Bearing'] = True
+    @Distance.setter
+    def Distance(self, distance):
+        self._distance = distance
 
-                    elif DataCamp['Bearing'] != None:
-                        ListSave.append(DataCamp)
-                        Bearing = lista[1].replace("  ", "").lstrip()
-                        DataCamp = {'Latitude': None, 'Longitude': None, 'Distance': None, 'Bearing': Bearing}
-                        Campadd = {'Latitude': False, 'Longitude': False, 'Distance': False, 'Bearing': True}
+    @property
+    def Bearing(self):
+        return self._bearing
 
-                    continue
+    @Bearing.setter
+    def Bearing(self, bearing):
+        self._bearing = bearing
 
-                elif not Campadd['Bearing']:
-                    Campadd['Bearing'] = True
 
-            if Campadd['Latitude'] and Campadd['Longitude'] and Campadd['Distance'] and Campadd['Bearing']:
-                ListSave.append(DataCamp)
-                DataCamp = {'Latitude': None, 'Longitude': None, 'Distance': None, 'Bearing': None}
-                Campadd = {'Latitude': False, 'Longitude': False, 'Distance': False, 'Bearing': False}
+class ETL:
 
-    return ListSave
+    def ExtracDataPoint(self,file) -> list:
+
+        dataPoint = DataPoint()
+        validateDataPoint = ValidateDataPoint()
+        ListSave = []
+
+        with codecs.open(file, "r", "utf-8") as readFileDataPoints:
+
+            ListDataPoints = [line.replace("\n", "").replace("Bearing", "\nBearing") for line in readFileDataPoints]
+
+            for line in ListDataPoints:
+                linha = line.split("\n")
+
+                for valor in linha:
+                    lista = valor.split(":")
+
+                    if lista[0] == "Latitude":
+
+                        if not validateDataPoint.Latitude:
+                            dataPoint.Latitude = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
+                            validateDataPoint.Latitude = True
+
+                        elif dataPoint.Latitude != None:
+
+                            ListSave.append(dataPoint)
+                            Latitude = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
+
+                            dataPoint = DataPoint(latitude=Latitude)
+                            validateDataPoint = ValidateDataPoint(latitude=True)
+
+                        continue
+
+                    elif not validateDataPoint.Latitude:
+                        validateDataPoint.Latitude = True
+
+                    if lista[0] == "Longitude":
+
+                        if not validateDataPoint.Longitude:
+                            dataPoint.Longitude = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
+                            validateDataPoint.Longitude = True
+
+                        elif dataPoint.Longitude != None:
+
+                            ListSave.append(dataPoint)
+                            Longitude = Decimal(lista[1].replace("  ", "").lstrip().split()[1])
+
+                            dataPoint = DataPoint(longitude=Longitude)
+                            validateDataPoint = ValidateDataPoint(longitude=True)
+
+                        continue
+
+                    elif not validateDataPoint.Longitude:
+                        validateDataPoint.Longitude = True
+
+                    if lista[0] == "Distance":
+
+                        if not validateDataPoint.Distance:
+                            dataPoint.Distance = lista[1].replace("  ", "").lstrip()
+                            validateDataPoint.Distance = True
+
+                        elif dataPoint.Distance != None:
+
+                            ListSave.append(dataPoint)
+                            Distance = lista[1].replace("  ", "").lstrip()
+
+                            dataPoint = DataPoint(distance=Distance)
+                            validateDataPoint = ValidateDataPoint(distance=True)
+
+                        continue
+
+                    elif not validateDataPoint.Distance:
+                        validateDataPoint.Distance = True
+
+                    if lista[0] == "Bearing":
+
+                        if not validateDataPoint.Bearing:
+                            dataPoint.Bearing = lista[1].replace("  ", "").lstrip()
+                            validateDataPoint.Bearing = True
+
+                        elif dataPoint.Bearing != None:
+                            ListSave.append(dataPoint)
+                            Bearing = lista[1].replace("  ", "").lstrip()
+
+                            dataPoint = DataPoint(bearing=Bearing)
+                            validateDataPoint = ValidateDataPoint(bearing=True)
+
+                        continue
+
+                    elif not validateDataPoint.Bearing:
+                        validateDataPoint.Bearing = True
+
+                if validateDataPoint.Latitude and validateDataPoint.Longitude and validateDataPoint.Distance and validateDataPoint.Bearing:
+                    ListSave.append(dataPoint)
+                    dataPoint = DataPoint()
+                    validateDataPoint = ValidateDataPoint()
+
+        return ListSave
--- application/AppGeo.py
+++ application/AppGeo.py
@@ -1,12 +1,10 @@
-import pandas as pd
-import numpy as np
 import logging
 import time
 from datetime import datetime
 from Download.PathFolderDownload import PathDownload
 from Useful.File import Files
 from Useful.GoogleMaps import GoogleMaps
-from RN.ETLDataPoints import ExtracDataPoint
+from RN.ETLDataPoints import ETL
 from Repository.FileManagement import FileManagement
 from Repository.Cidade import Cidade
 from Repository.Estado import Estado
@@ -15,79 +13,78 @@ from Repository.Logradouro import Logradouro
 from Repository.Pais import Pais
 from Repository.Points import Points
 
-#Apenas executar esse arquivo, já está tudo configurado para extrair os arquivos e coletar.
-def ExtractGeo():
-    # Essa Função tem objecto da extração da aplicação em volta do projeto
-    # Onde tem toda a validação de não repetir valores e salvar os valores no banco de dados
-    # Configurações de parametro de Banco ficam no arquivo /DAO/DataBase
-    # CHave GoogleMaps continuara Ativa até 01/08/2019
 
-    path = PathDownload()
-    file = Files(directory=path)
-    maps = GoogleMaps(ApiKey='AIzaSyBOfvtPquH05kbxAlnSo4eJoLdHGuP3lZk', language='pt-BR')
+# Configurações de parametro de Banco ficam no arquivo /DAO/DataBase
+# CHave GoogleMaps continuara Ativa até 01/08/2019
 
-    fileManagement = FileManagement()
-    points = Points()
-    pais = Pais()
-    estado = Estado()
-    cidade = Cidade()
-    logradouro = Logradouro()
-    identificationPoint = IdentificationPoint()
+# Essa Classe é extração da aplicação em volta do projeto.
+# Onde tem toda a validação de não repetir valores e salvar os valores no banco de dados
+class AppGeo:
 
-    ListFile = file.ReturnExtractTazGz(url='https://s3.amazonaws.com/dev.etl.python/datasets/data_points.tar.gz')
+    def ExtractGeo(self):
+        path = PathDownload()
+        file = Files(directory=path)
+        maps = GoogleMaps(ApiKey='AIzaSyBOfvtPquH05kbxAlnSo4eJoLdHGuP3lZk', language='pt-BR')
 
-    for filedatapoints in ListFile:
+        # Repositorios
+        fileManagement = FileManagement()
+        points = Points()
+        pais = Pais()
+        estado = Estado()
+        cidade = Cidade()
+        logradouro = Logradouro()
+        identificationPoint = IdentificationPoint()
 
-        if not fileManagement.CheckExistingFile(filedatapoints):
-            IDFile = fileManagement.Save(NomeArquivo=filedatapoints, CreateFile=datetime.now(), id=True)
+        ListFile = file.ReturnExtractTazGz(url='https://s3.amazonaws.com/dev.etl.python/datasets/data_points.tar.gz')
+        for filedatapoints in ListFile:
 
-            for datapoint in ExtracDataPoint(filedatapoints):
+            if not fileManagement.CheckExistingFile(filedatapoints):
+                IDFile = fileManagement.Save(NomeArquivo=filedatapoints, CreateFile=datetime.now(), id=True)
 
-                if datapoint['Latitude'] != None and datapoint['Longitude'] != None:
+                for datapoint in ETL().ExtracDataPoint(filedatapoints):
 
-                    if not points.CheckExistingPoint(datapoint['Latitude'], datapoint['Longitude']):
-                        IDPoint = points.Save(Latitude=datapoint['Latitude'], Logintude=datapoint['Longitude'],
-                                              Distance=datapoint['Distance'], Bearing=datapoint['Bearing'],
-                                              IDFile=IDFile,
-                                              id=True)
+                    if datapoint.Latitude != None and datapoint.Longitude != None:
 
-                        coordenadas = (datapoint['Latitude'], datapoint['Longitude'])
-                        maps.SearchGeoCode(coordenadas)
+                        if not points.CheckExistingPoint(datapoint.Latitude, datapoint.Longitude):
+                            IDPoint = points.Save(Latitude=datapoint.Latitude, Logintude=datapoint.Longitude,
+                                                  Distance=datapoint.Distance, Bearing=datapoint.Bearing,
+                                                  IDFile=IDFile,
+                                                  id=True)
 
-                        IDPais = pais.CheckExistingPais(maps.CountryLong)
-                        if not IDPais:
-                            IDPais = pais.Save(Nome=maps.CountryLong, Sigla=maps.CountryShort, id=True)
+                            coordenadas = (datapoint.Latitude, datapoint.Longitude)
+                            maps.SearchGeoCode(coordenadas)
 
-                        IDEstado = estado.CheckExistingPais(maps.StateLong)
-                        if not IDEstado:
-                            IDEstado = estado.Save(Nome=maps.StateLong, Sigla=maps.StateShort, IDPais=IDPais, id=True)
+                            IDPais = pais.CheckExistingPais(maps.CountryLong)
+                            if not IDPais:
+                                IDPais = pais.Save(Nome=maps.CountryLong, Sigla=maps.CountryShort, id=True)
 
-                        IDCidade = cidade.CheckExistingCidade(maps.City)
-                        if not IDCidade:
-                            IDCidade = cidade.Save(Nome=maps.City, IDEstado=IDEstado, id=True)
+                            IDEstado = estado.CheckExistingPais(maps.StateLong)
+                            if not IDEstado:
+                                IDEstado = estado.Save(Nome=maps.StateLong, Sigla=maps.StateShort, IDPais=IDPais,
+                                                       id=True)
 
-                        IDlogradouro = logradouro.CheckExistingLogradouro(maps.Street, maps.Number, maps.District,
-                                                                          maps.PostalCode)
-                        if not IDlogradouro:
-                            IDlogradouro = logradouro.Save(Rua=maps.Street, Numero=maps.Number, bairro=maps.District,
-                                                           CEP=maps.PostalCode, IDCidade=IDCidade, id=True)
+                            IDCidade = cidade.CheckExistingCidade(maps.City)
+                            if not IDCidade:
+                                IDCidade = cidade.Save(Nome=maps.City, IDEstado=IDEstado, id=True)
 
-                        IDGeo = identificationPoint.CheckExistingLogradouro(IDPoint, IDPais, IDEstado, IDCidade,
-                                                                            IDlogradouro)
-                        if not IDGeo:
-                            identificationPoint.Save(IDPoint, IDPais, IDEstado, IDCidade, IDlogradouro)
+                            IDlogradouro = logradouro.CheckExistingLogradouro(maps.Street, maps.Number, maps.District,
+                                                                              maps.PostalCode)
+                            if not IDlogradouro:
+                                IDlogradouro = logradouro.Save(Rua=maps.Street, Numero=maps.Number,
+                                                               bairro=maps.District,
+                                                               CEP=maps.PostalCode, IDCidade=IDCidade, id=True)
 
-                        logging.info("%s %s - %s - %s %s - %s %s - %s ", maps.Street, maps.Number, maps.District,
-                                     maps.StateLong, maps.StateShort, maps.CountryLong, maps.CountryShort,
-                                     maps.PostalCode)
+                            IDGeo = identificationPoint.CheckExistingLogradouro(IDPoint, IDPais, IDEstado, IDCidade,
+                                                                                IDlogradouro)
+                            if not IDGeo:
+                                identificationPoint.Save(IDPoint, IDPais, IDEstado, IDCidade, IDlogradouro)
 
+                            logging.info("%s %s - %s - %s %s - %s %s - %s ", maps.Street, maps.Number, maps.District,
+                                         maps.StateLong, maps.StateShort, maps.CountryLong, maps.CountryShort,
+                                         maps.PostalCode)
 
-    result = identificationPoint.VisualizacaoFINAL()
-    logging.info("%s",result)
-
-    # df = pd.DataFrame(data=np.asarray(result),
-    #                   columns=['Latitude', 'Logintude', 'Rua', 'Numero', 'bairro', 'Cidade', 'CEP', 'Estado', 'Pais'])
-    # print(df.head())
+        result = identificationPoint.VisualizacaoFINAL()
+        logging.info("%s", result)
 
 
 if __name__ == '__main__':
@@ -95,7 +92,7 @@ if __name__ == '__main__':
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
-    ExtractGeo()
+    AppGeo().ExtractGeo()
 
     logging.info("Tempo de execução do script: %s segundos" % (time.time() - start_time))
     logging.info("Script executado com sucesso!")
